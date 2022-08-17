013823a4bbae4b4b6202daeeb71c8b5a5fa3b1cc
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-08-17 08:50:13 -0400

    🐸 initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        loader: "akamai",
+        path: ".",
+    },
 }
 
 module.exports = nextConfig
